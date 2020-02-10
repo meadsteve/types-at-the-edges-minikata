4b7de0c7d99d5899b02402bdd88dba964ac6ff8b
steve <meadsteve@gmail.com>
2020-02-10 09:30:06 +0100

    add some really basic tests

--- diary/test/test_everything.py
+++ diary/test/test_everything.py
@@ -1,3 +1,21 @@
+from collections import defaultdict
 
-def test_starting_is_okay():
-    assert True
+from diary.indexer import add_to_title_index, DiaryEntry, add_to_category_index, TitleIndex, CategoryIndex
+
+
+def test_titles_have_white_space_stripped():
+    entry = DiaryEntry(title=" hello   ", body="", category="general", entry_ts="2020-01-09 12:22")
+    index: TitleIndex = defaultdict(list)
+    add_to_title_index(index, entry)
+
+    assert len(index[" hello   "]) == 0
+    assert len(index["hello"]) == 1
+
+
+def test_categories_are_lower_cased():
+    entry = DiaryEntry(title="hello", body="", category="GENERAL", entry_ts="2020-01-09 12:22")
+    index: CategoryIndex = defaultdict(list)
+    add_to_category_index(index, entry)
+
+    assert len(index["GENERAL"]) == 0
+    assert len(index["general"]) == 1
