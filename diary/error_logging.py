3f2e0b5f0f380653594c255b35cf2208ebcd31b9
steve <meadsteve@gmail.com>
2020-02-10 10:26:25 +0100

    whoops

--- diary/error_logging.py
+++ diary/error_logging.py
@@ -1,8 +1,10 @@
+from typing import Any
+
 from pydantic import ValidationError
 
 
 class DataError(RuntimeError):
 
-    def __init__(self, value: any, pydantic_error: ValidationError):
+    def __init__(self, value: Any, pydantic_error: ValidationError):
         self.value = value
         self.validation_error = pydantic_error
