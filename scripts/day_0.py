27d2b59fa122074c68e8c86f62ae0f7c655fdf6c
steve <meadsteve@gmail.com>
2020-09-21 20:11:58 +0200

    dry up day zero as well

--- scripts/day_0.py
+++ scripts/day_0.py
@@ -1,8 +1,7 @@
-from diary.indexer import run_indexing
+from scripts.common import handle_error, run
 
 if __name__ == "__main__":
-    indexed = run_indexing("dev")
-    print("Successfully indexed data")
-    print(f"{len(indexed.entries)} entries")
-    print(f"{len(indexed.title_index.keys())} unique titles")
-    print(f"{len(indexed.category_index.keys())} unique categories")
+    try:
+        run("dev")
+    except Exception as error:
+        handle_error(error, "Erm... We didn't really expect it to fail in dev")
