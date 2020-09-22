dc50e12063f7489553cbf58416a1d3363ce76c18
steve <meadsteve@gmail.com>
2020-09-22 18:51:48 +0200

    oops made the 3rd day the same as the second

--- scripts/day_3.py
+++ scripts/day_3.py
@@ -2,7 +2,7 @@ from scripts.common import run, handle_error
 
 if __name__ == "__main__":
     try:
-        run("the-second-day")
+        run("the-third-day")
     except Exception as ooops:
         extra_detail = \
             "We spoke to the owners of the API and they've told us that users can save 'in draft' entries.\n" \
